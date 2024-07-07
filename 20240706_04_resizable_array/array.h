a1a8794b2d7650141cdb93a1c7e67209aa2c9c83
angelra1994 <2641138638@qq.com>
2024-07-07 16:58:39 +0800

    构造一个linked_list

--- 20240706_04_resizable_array/array.h
+++ 20240706_04_resizable_array/array.h
@@ -1,6 +1,22 @@
 #ifndef _ARRAY_H_
 #define _ARRAY_H_
 
+/**
+ * provide a mechanism of resizable array of int
+ * 1. Growable
+ * 2. Get the current size
+ * 3. Access to the elements 
+ * 
+ * Array array_create(int init_size);
+ * void array_free(Array *a);
+ * int array_size(const Array *a);
+ * int* array_at(Array *a, int index);
+ * void array_inflate(Array *a, int more_size);
+ * int array_get(const Array *a, int index);
+ * void array_set(Array *a, int index, int value);
+ */
+
+
 const int BLOCK_SIZE = 20;
 
 typedef struct {
---
+++ 20240706_05_linked_list/linked_list.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "node.h"
+
+// typedef struct _node {
+//     int value;
+//     struct _node *next;
+// } Node;
+
+
+int main(int argc, char const *argv[])
+{
+
+    Node * head = NULL;
+    int number;
+    do {
+        scanf("%d", &number);
+        if (number != -1) {
+            // add to linked_list
+            Node *p = (Node*) malloc(sizeof(Node));
+            p->value = number;
+            p->next = NULL;
+
+            // find the last
+            Node *last = head;
+            if (last) {
+                while ( last->next )
+                {
+                    last = last -> next;
+                }  
+                // attach
+                last->next = p;
+            } else { // list 是 空的场景
+                head = p;
+            }
+        }
+    } while (number != -1);
+    return 0;
+}
