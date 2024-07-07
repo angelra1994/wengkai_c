7a087ed15625a0b73b6704f5237af3ffcf4f7cdc
angelra1994 <2641138638@qq.com>
2024-07-07 18:10:10 +0800

    数组的操作

--- 20240706_05_linked_list/linked_list.c
+++ 20240706_05_linked_list/linked_list.c
@@ -7,33 +7,87 @@
 //     struct _node *next;
 // } Node;
 
+typedef struct _lsit {
+    Node* head;
+    // Node* tail;
+} List;
+
+
+
+void add(List* pList, int number);
 
 int main(int argc, char const *argv[])
 {
-
-    Node * head = NULL;
+    List list;
+    list.head = NULL;
+    // list.head = list.tail = NULL;
     int number;
     do {
         scanf("%d", &number);
         if (number != -1) {
-            // add to linked_list
-            Node *p = (Node*) malloc(sizeof(Node));
-            p->value = number;
-            p->next = NULL;
-
-            // find the last
-            Node *last = head;
-            if (last) {
-                while ( last->next )
-                {
-                    last = last -> next;
-                }  
-                // attach
-                last->next = p;
-            } else { // list 是 空的场景
-                head = p;
-            }
+            add(&list, number);
         }
     } while (number != -1);
+
+    print(&list);
+
+    scanf("%d", &number);
+    Node *p;
+    int isFound = 0;
+    for ( p=list.head; p; p = p->next) {
+        if (p->value == number) {
+            printf("找到了\n");
+            isFound = 1;
+            break;
+        }
+    }
+    if (!isFound) {
+        printf("没找到\n");
+    }
+
+    Node *q;
+    for (q=NULL, p=list.head; p; q=p, p = p->next) {
+        if (p->value == number) {
+            if (q) {
+                // 代码中出现可能左值为null的情况就是边界条件。Any pointer at the left of -> nust be checked
+                q->next = p->next;
+            } else {
+                list.head = p->next;
+            }
+            free(p);
+            break;
+        }
+    }
+
     return 0;
+}
+
+void add(List* pList, int number)
+{ 
+    // add to linked_list
+    Node *p = (Node *)malloc(sizeof(Node));
+    p->value = number;
+    p->next = NULL;
+
+    // find the last
+    Node *last = pList->head;
+    if (last) {
+        while (last->next)
+        {
+            last = last->next;
+        }
+        // attach
+        last->next = p;
+    } else { // list 是 空的场景
+        pList->head = p;
+    }
+}
+
+void print(List *pList)
+{
+    Node* p;
+    for ( p=pList->head; p; p = p->next) {
+        printf("%d\t", p->value);
+    }
+    printf("\n");
 }
